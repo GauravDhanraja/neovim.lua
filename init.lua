663b75fbb26702d4f639476f24a69327aa92212f
GauravDhanraja <gauravdhanraja88@proton.me>
2024-06-23 23:44:23 +0530

    keymappings and other vim options moved to seperate file in /lua/vim-options.lua

--- init.lua
+++ init.lua
@@ -1,33 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.undofile = true
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = -1
-vim.opt.shiftwidth = 4
-
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes'
-
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.breakindent = true
-
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -52,3 +25,4 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup("plugins")
+require("vim-options")
---
+++ lua/vim-options.lua
@@ -0,0 +1,30 @@
+vim.opt.undofile = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.breakindent = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")

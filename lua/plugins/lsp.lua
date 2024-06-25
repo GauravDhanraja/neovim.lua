08388e1c1e630b9344041a92ce2b476a854dfe22
GauravDhanraja <gauravdhanraja88@proton.me>
2024-06-25 17:36:16 +0530

    Add lsp_signature.nivm for r_language_server support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,14 @@ return {
         },
     },
     {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts)
+            require("lsp_signature").setup(opts)
+        end,
+    },
+    {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
@@ -46,8 +54,6 @@ return {
             })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
